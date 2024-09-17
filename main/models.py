1ee0b82183818a446fbd48a4fca92536b1403ac7
thomascibersport <thomascibersport@gmail.com>
2024-09-17 09:51:49 +0300

    добавления видео в админке

--- main/models.py
+++ main/models.py
@@ -2,8 +2,3 @@ from django.db import models
 
 # Create your models here.
 
-class Video(models.Model):
-    title = models.CharField(max_length=150)
-    description = models.TextField()
-    thumbnail = models.ImageField(upload_to="videos/thumbnails")
-    source = models.FileField(upload_to="videos")
