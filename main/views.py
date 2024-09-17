773f8a75a8d147a4c8abcb8c6a974fc17a4a63c2
thomascibersport <thomascibersport@gmail.com>
2024-09-17 08:25:22 +0300

    Наша первая версия

---
+++ main/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
+
+# Create your views here.
+
+def home_page(req: HttpRequest) -> HttpResponse:
+    return HttpResponse("Home page")
