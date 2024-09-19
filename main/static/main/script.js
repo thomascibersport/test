6d2e7d24fc43b2c3609ccba87ca8b8b389b410b9
root <thomascibersport@gmail.com>
2024-09-19 08:34:09 +0300

    раздали стилей видео

--- main/static/main/script.js
+++ main/static/main/script.js
@@ -1,1 +1,13 @@
 console.log('Hello, world')
+
+video = document.getElementById("video")
+let body = document.body; // добавьте эту строку
+
+function btn_video() {
+    if(video) { // проверка на существование видео
+        video.style.display = 'block';
+    } else {
+        console.error("Video element not found.");
+    }
+}
+
