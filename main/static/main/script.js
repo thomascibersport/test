942d064ab52d63e275aa6e182af565e258cd6ca2
root <thomascibersport@gmail.com>
2024-09-19 08:51:16 +0300

    раздали стилей видео №2

--- main/static/main/script.js
+++ main/static/main/script.js
@@ -1,13 +1,16 @@
 console.log('Hello, world')
 
-video = document.getElementById("video")
-let body = document.body; // добавьте эту строку
+var modal = document.getElementById('loginModal');
+var btn = document.getElementById('btn_video');
+var video = document.getElementById('video');
 
-function btn_video() {
-    if(video) { // проверка на существование видео
-        video.style.display = 'block';
-    } else {
-        console.error("Video element not found.");
+btn.addEventListener('click', function() {
+    modal.style.display = 'block';
+});
+window.addEventListener('click', function(event) {
+    if(event.target == modal) {
+        modal.style.display = 'none';
+        video.pause();
+        video.currentTime = 0;
     }
-}
-
+});
