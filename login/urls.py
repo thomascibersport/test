0a2e8bd840005962bf5771bc075d9f4cd4bad01d
root <thomascibersport@gamil.com>
2024-09-23 13:20:48 +0300

    сохранние куки и кнопка выходы

--- login/urls.py
+++ login/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from  . import views
 urlpatterns =[
     path('login/', views.login_view),
-    path('register/', views.register)
+    path('register/', views.register),
+    path('logout/', views.logout_view)
 ]
--- login/views.py
+++ login/views.py
@@ -1,12 +1,27 @@
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
 
 
 def login_view(request:HttpRequest) -> HttpResponse:
     if request.method == 'POST':
-        return redirect('/')
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            login(request, user)
+            return redirect("/")
+        else:
+            return redirect('/login')
+
     return  render(request, 'login/login.html')
 
 
 def register(request:HttpRequest) -> HttpResponse:
     return render(request,'login/register.html')
+
+def logout_view(request:HttpRequest) -> HttpResponse:
+    logout(request)
+    return redirect('/')
